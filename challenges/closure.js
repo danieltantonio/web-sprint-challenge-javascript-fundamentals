6d81b4fbb197bae0be0b5b06d69a5e3617b1ec36
Daniel Antonio <danieltomantonio@gamil.com>
2020-06-19 09:07:12 -0700

    Finished all challenges and sprint challenges

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,19 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation: The reason why the nested function can access the internal variable is because of the block scope nature of the const variable keyword. It can only access things upwards but never downwards.
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function summation(num) {
+  let sum = 0;
+  for(let i = 1; i <= num; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
+
+console.log(summation(4));
